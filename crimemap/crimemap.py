863018082c3e4eafb2b4ced40e9c2835f9d52ca3
John Morrice <john@functorama.com>
2016-09-15 10:55:48 +0100

    Click integration to populate app database

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,5 +1,12 @@
+import click
 from flask import Flask
+
 app = Flask(__name__)
+
+@app.cli.command(help='Read data file and init database')
+@click.argument('filepath', nargs=1)
+def initdb(filepath):
+	click.echo('Should read ' + filepath)
 
 @app.route('/api/dates')
 def dates():
@@ -10,11 +17,11 @@ def countries():
 	return '[]'
 
 @app.route('/api/countries/<country>')
-def country():
+def country(country):
 	return '{}'
 
 @app.route('/api/countries/<country>/<date>')
-def crountry_date():
+def crountry_date(country, date):
 	return '0'
 
 @app.route('/')
